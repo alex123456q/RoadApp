b7544278f1730d8102abf26214f0d8ade17bec88
Alexandra <sasha-st-95@inbox.ru>
2015-12-22 03:51:03 +0300

    To linear. smth strange. strange with vector claster before exit and after.

--- RoadDataApp.cpp
+++ RoadDataApp.cpp
@@ -13,10 +13,15 @@
 #include <fstream>
 #include <map>
 #include <cmath>
+#include <tuple>
 
 struct myPoint{
     RoadPoint p;
     int numb_clast;
+    myPoint(){
+    p.x = 0;
+    p.y = 0;
+    p.z = 0;};
     myPoint(RoadPoint* _p, int _numb_clast = -1):
     numb_clast(_numb_clast)
     {
@@ -53,100 +58,134 @@ function IntersectRect(r1:Rectangle, r2:Rectangle):Boolean {
 }
 */
 //BOUNDING BOX
-#define A 0.49
+#define A 5.0//0.49
 #define H 1
 #define kap 0.5
-void createBoundingBox(std::vector<myPoint*>& points){
+std::pair<myPoint, myPoint> createBoundingBox(std::vector<myPoint*>& points){
     auto xExtremes = std::minmax_element(points.begin(), points.end(),
                                          [](const myPoint* lhs, const myPoint* rhs) {
-                                             return lhs->p.x < lhs->p.x;
+                                             return lhs->p.x < rhs->p.x;
                                          });
 
     auto yExtremes = std::minmax_element(points.begin(), points.end(),
                                          [](const myPoint* lhs, const myPoint* rhs) {
-                                             return lhs->p.y < lhs->p.y;
+                                             return lhs->p.y < rhs->p.y;
                                          });
     auto zExtremes = std::minmax_element(points.begin(), points.end(),
                                          [](const myPoint* lhs, const myPoint* rhs) {
-                                             return lhs->p.z < lhs->p.z;
+                                             return lhs->p.z < rhs->p.z;
                                          });
-
+    
     myPoint Left((*xExtremes.first)->p.x, (*yExtremes.first)->p.y, (*zExtremes.first)->p.z, points[0]->numb_clast);
     myPoint Right((*xExtremes.second)->p.x, (*yExtremes.second)->p.y, (*zExtremes.second)->p.z, points[0]->numb_clast);
+    return std::make_pair(Left, Right);
 }
 
 bool checkBox(myPoint Left, myPoint Right){
     double Lx = fabs(Right.p.x - Left.p.x);
     double Ly = fabs(Right.p.y - Left.p.y);
     double Lz = fabs(Right.p.z - Left.p.z);
+    std::cout << "LLLL " << Lx << " "<<  Ly << " "<< Lz << std::endl;
     return (Lx*Ly < A && Lz >= kap * H);
 }
 
-#define R 10     //ширина поиска локальных сгущений - входной параметр алгоритма
+#define R 6     //ширина поиска локальных сгущений - входной параметр алгоритма
 
 double squaredDistance(myPoint* a, myPoint* b){
     return pow(a->p.x - b->p.x, 2) + pow(a->p.y-b->p.y, 2) + pow(a->p.z-b->p.z, 2);
 };
 
-int CenterObject(std::vector<myPoint*>& points, std::vector<int>& indexes, std::vector<int>& curclast){
-    double mindist = 1000000000.0;
-    double curdist;
-    int minindex = -1;
+myPoint CenterObject(std::vector<myPoint*>& points, std::vector<int>& indexes, std::vector<int>& curclast){
+    //double mindist = 1000000000.0;
+    //double curdist;
+    //int minindex = -1;
+    myPoint centre;
     for (int i = 0; i < curclast.size(); ++i){
-        curdist = 0.0;
-        for (int j = 0; j < curclast.size(); ++j)
+        centre.p.x += points[indexes[curclast[i]]]->p.x;
+        centre.p.y += points[indexes[curclast[i]]]->p.y;
+        centre.p.z += points[indexes[curclast[i]]]->p.z;
+    /*    for (int j = 0; j < curclast.size(); ++j)
             curdist += sqrt(squaredDistance(points[indexes[curclast[i]]], points[indexes[curclast[j]]]));
         if (mindist > curdist){
             mindist = curdist;
             minindex = indexes[curclast[i]];
-        }
+        }*/
     }
-    return minindex;//minpoint;
+    centre.p.x /= curclast.size();
+    centre.p.y /= curclast.size();
+    centre.p.z /= curclast.size();
+    return centre;//minindex;
 };
 
-void genClaster(std::vector<myPoint*>& points, std::vector<int>& indexes, int center, std::vector<int>* curclast){
+void genClaster(std::vector<myPoint*>& points, std::vector<int>& indexes, myPoint center, std::vector<int>* curclast){
     for (int i = 0; i < indexes.size(); ++i){
-        if (squaredDistance(points[indexes[i]], points[center]) < R*R)
+        if (squaredDistance(points[indexes[i]], &center) < R*R)
             curclast->push_back(i);
     }
 }
+/*
+void addToClaster((std::vector<myPoint*>& points, std::vector<int>& indexes, int center, std::vector<int>* curclast){
+    int j = 0;
+    int sz = curlast.size();
+    for (int i = 0; i < sz; ++i){
+        while (j < curlast[i]){
+            if (squaredDistance(points[indexes[j]], points[center]) < R*R)
+                curclast->push_back(j);
+            ++j;
+        }
+    }
+}*/
 
-//void addToClaster((std::vector<myPoint*>& points, std::vector<int>& indexes, int center, std::vector<int>* curclast){
-//    for (int i = 0; i < indexes.size(); ++i){
-//        if (squaredDistance(points[indexes[i]], points[center]) < R*R)
-//            curclast->push_back(i);
-//    }
-//}
+struct myClast{
+    std::pair<myPoint, myPoint> box;
+    std::vector<myPoint*> elems;
 
-int FOREL(std::vector<myPoint*>& allpoints){
+    myClast(){};
+};
+int FOREL(std::vector<myPoint*>& allpoints, std::vector< myClast > * clasters){
     std::vector<int> indexes;
     for (int i = 0; i < allpoints.size(); ++i)
         indexes.push_back(i);
     std::vector<int> curclast;
-    int predcenter, curcenter, j = -1;
+    myPoint predcenter, curcenter;
+    int j = -1;
     int numClast = 0;
     while (indexes.size() > 0){
         curclast.clear();
         std::cout << indexes.size() <<std::endl;
-        predcenter = indexes[rand()%indexes.size()];
+        predcenter = *allpoints[indexes[rand()%indexes.size()]];
         genClaster(allpoints, indexes, predcenter, &curclast);
         curcenter = CenterObject(allpoints, indexes, curclast);
-        while (squaredDistance(allpoints[curcenter], allpoints[predcenter]) > 5){
-            std::cout << curcenter <<" " << predcenter << " " << curclast.size() << std::endl;
+        while (squaredDistance(&curcenter, &predcenter) > 0.00001){
+            //std::cout << curcenter.p.x <<" " << predcenter.p.x << " " << curclast.size() << std::endl;
             predcenter = curcenter;
             curclast.clear();
             genClaster(allpoints, indexes, predcenter, &curclast);
             curcenter = CenterObject(allpoints, indexes, curclast);
         }
         std::vector<int>::iterator it = indexes.begin();
+        //std::vector<myPoint*> clastpoints;
+        if (clasters->size() < numClast + 1)
+            clasters->resize(numClast + 1);
         for (int i = 0; i < curclast.size(); ++i){
             j = indexes[curclast[i]];
             //std::cout << j;
             indexes.erase(it + curclast[i] - i);
             allpoints[j]->numb_clast= numClast;                  //+height*10
+            (*clasters)[numClast].elems.push_back(allpoints[j]);
+            //вообще можно, наверное, взять макс и мин значения иксов и иже с ними ручками
+        }
+        std::cout << "SIZEofclaster " << (*clasters)[numClast].elems.size()<<std::endl;
+        //std::pair<myPoint, myPoint> box = createBoundingBox((*clasters)[numClast].elems);
+        (*clasters)[numClast].box = createBoundingBox((*clasters)[numClast].elems);
+        std::pair<myPoint, myPoint> box = createBoundingBox((*clasters)[numClast].elems);
+        if (!checkBox(std::get<0>(box), std::get<1>(box))){
+            //(*clasters)[numClast].elems.clear();
+            //--numClast;
         }
         ++numClast;
     }
+    std::cout << "before exit " << clasters->size() << std::endl;
     return numClast;
 };
 
@@ -201,18 +240,19 @@ int _tmain(int argc, _TCHAR* argv[])
     }
     //std::cout << "All" << allpoints.size()<<std::endl;
 
-    std::map<int, std::vector< std::vector<myPoint*> > > clasters;
+    std::map<int, std::vector< myClast > > clasters;
 
     for (std::map<int, std::vector<myPoint*> >::iterator it = heights.begin(); it != heights.end(); ++it){
-        std::cout <<it->first <<" "<<FOREL(it->second) << std::endl;
+        std::cout <<it->first <<" "<<FOREL(it->second, &clasters[it->first]) << " " << clasters[it->first].size() << std::endl;
         //std::cout << it->second.size() << std::endl;
+        if (it->first == 22){
         for (int k = 0; k < it->second.size(); ++k){
             dlib::vector<double> val(it->second[k]->p.x, it->second[k]->p.y, it->second[k]->p.z);
             // Pick a color based on how far we are along the spiral
-            dlib::rgb_pixel color = dlib::colormap_jet(it->second[k]->numb_clast,0,5);
+            dlib::rgb_pixel color = dlib::colormap_jet(it->second[k]->numb_clast,0,6);
             // And add the point to the list of points we will display
             pointsPaint.push_back(dlib::perspective_window::overlay_dot(val, color));
-        }
+        } }
             //std::cout<<it->second[k]->numb_clast;
     }
    /* for (std::map<int, std::vector<myPoint*> >::iterator it = heights.begin(); it != heights.end(); ++it){
